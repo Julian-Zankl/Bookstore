79cf911444c6c12779d278f02f221068be269350
Julian Zankl <julian.zankl@clouddna.at>
2025-04-23 22:27:59 +0200

    added metrics

--- csr/src/app/pages/book-detail/book-detail.component.ts
+++ csr/src/app/pages/book-detail/book-detail.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 import { BookDetailComponent } from "../../components/book-detail/book-detail.component";
 
 @Component({
   standalone: true,
-  selector: 'app-book-detail-page',
+  selector: "app-book-detail-page",
   template: '<app-book-detail [id]="id"></app-book-detail>',
   imports: [BookDetailComponent],
 })
 export class BookDetailPage {
-  @Input({required: true}) id!: string;
+  @Input({ required: true })
+  id!: string;
 }
